0ca27b2116a94988d473c56bb8b6afeb1afdc41e
khoadtk <khoadtk98@gmail.com>
2021-04-05 15:51:31 +0700

    edit reponsive, add dark to light

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const lci = document.querySelector(".location");
 const service = document.querySelector(".service__card");
 const shop = document.querySelector(".shop");
 const product = document.querySelector(".product");
+const content = document.querySelector(".content");
 const nav = document.querySelectorAll(".nav__item");
 const barItem = document.querySelectorAll(".content__bar--item");
 const productItem = document.querySelectorAll(".shop__bar--item");
@@ -12,29 +13,64 @@ const bgBottom = document.querySelector(".main__bottom--img");
 const app = document.querySelector(".app");
 const productList = document.querySelector(".product__list");
 
+const btnHome=document.getElementById("home")
 
+
+//dark to light
+document.querySelector(".mobile--dark").addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+  });
+
+//loadding page
+function loadding(){
+    if(window.innerWidth<739){
+        btnHome.innerHTML="Home"
+    }
+}
 
 //show Home page
 function toPage() {
-    arrMain.forEach((e) => (e.style.display = "none"));
+    arrMain.forEach((e) => {
+        (e.style.display = "none")
+    });
     if (this.innerText === "Service") {
+        console.log("service")
         main.style.display = "block";
         service.style.display = "block";
+        content.style.display = "none";
         app.style.backgroundImage = "url(asset/Background02.png)";
         bgBottom.src = "asset/service__bottom.png";
+        if (window.innerWidth<739) {
+            document.querySelector(".nav__menu").style.display="none";
+            document.querySelector(".nav__menu--mobile").style.display="flex";
+        }
     } else if (this.innerText === "Shop") {
         main.style.display = "block";
         shop.style.display = "flex";
+        content.style.display = "none";
         app.style.backgroundImage = "url(asset/Background07.png)";
         bgBottom.src = "asset/service__bottom.png";
+        if (window.innerWidth<739) {
+            document.querySelector(".nav__menu").style.display="none";
+            document.querySelector(".nav__menu--mobile").style.display="flex";
+        }
     } else if (this.innerText === "Location") {
         app.style.backgroundImage = "url(asset/Background08.png)";
         main.style.display = "block";
         lci.style.display = "block";
         bgBottom.src = "asset/service__bottom.png";
+        if (window.innerWidth<739) {
+            document.querySelector(".nav__menu").style.display="none";
+            document.querySelector(".nav__menu--mobile").style.display="flex";
+        }
     } else {
         home.style.display = "block";
         app.style.backgroundImage = "url(asset/Background01.png)";
+        content.style.display = "block";
+        if (window.innerWidth<739) {
+            document.querySelector(".nav__menu").style.display="none";
+            document.querySelector(".nav__menu--mobile").style.display="flex";
+        }
     }
 }
 
@@ -43,6 +79,7 @@ function toProduct() {
     app.style.backgroundImage = "url(asset/Background02.png)";
     main.style.display = "block";
     product.style.display = "flex";
+    content.style.display = "none";
     bgBottom.src = "asset/service__bottom1.png";
     window.scrollTo(0, 0);
     console.log(bgBottom);
